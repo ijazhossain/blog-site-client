450e4561718c638e118f6fa9e823379674cc9133
ijazhossain <ijaz.hossain35@gmail.com>
2026-01-19 23:44:53 +0600

    proxy file added

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 import { userService } from "../services/user.service";
 
 export default async function Home() {
-  const {data,error}=await userService.getSession();
-  console.log({data,error});
+  // const {data,error}=await userService.getSession();
+  // console.log({data,error});
   return (
     <div>
       <Button variant="outline">Click here</Button>
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,20 @@
+import { env } from "@/env";
 import { cookies } from "next/headers";
+const AUTH_URL = env.AUTH_URL;
+// console.log({AUTH_URL});
 export const userService = {
   getSession: async function () {
     try {
       const cookieStore = await cookies();
-      // console.log(cookieStore.toString());
-      const res = await fetch("http://localhost:5000/api/auth/get-session", {
+      //   console.log(cookieStore.toString());
+      const res = await fetch(`${AUTH_URL}/get-session`, {
         headers: {
           Cookie: cookieStore.toString(),
         },
         cache: "no-store",
       });
       const session = await res.json();
-      if (session.data===null) {
+      if (session?.data === null) {
         return { data: null, error: { message: "Session is missing" } };
       }
       return { data: session, error: null };
